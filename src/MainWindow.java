d64726c4cb9564efd8120d9ecb67c1f593387108
Mollonn <35244441+Mollonn@users.noreply.github.com>
2018-01-16 05:08:06 +0200

    gratz it works

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -9,7 +9,7 @@ public class MainWindow extends JFrame
         setTitle("Snake");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(370,390);
-        setLocation(400,400);
+        setLocation(1920/2-370/2,1080/2-390/2);
         add(new GameField());
         setVisible(true);
     }
