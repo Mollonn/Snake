a751370b1fa38eb61f0f21c60db5257ebd9fadcf
Mollonn <vadik.medvedyuk@gmail.com>
2018-01-18 21:38:25 +0200

    Add files via upload

--- src/GameField.java
+++ src/GameField.java
@@ -13,7 +13,9 @@ public class GameField extends JPanel implements ActionListener
     private boolean right = true;
     private boolean up = false;
     private boolean down = false;
-    private Image dot;
+    private Image dot_chrome;
+    private Image dot_ya;
+    private Image dot_fire;
     private Image apple;
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
@@ -25,82 +27,114 @@ public class GameField extends JPanel implements ActionListener
     private int snake_segment;
     private Timer Time;
     private boolean inGame = true;
-
+    private int score;
 
     public GameField()
     {
-        setBackground(Color.DARK_GRAY);
+        setBackground(Color.GRAY);
         loadImages();
-        initGame();
         addKeyListener(new FieldKeyListener());
         setFocusable(true);
+        InitGame();
 
     }
 
-    public void initGame()
+    public void InitGame()
     {
-        snake_segment = 4;
+        snake_segment = 3;
         for (int i = 0; i < snake_segment; i++)
         {
-            x[i] = DOT_SIZE*3;
-            y[i] = DOT_SIZE*3;
+            x[i] = DOT_SIZE * 3;
+            y[i] = DOT_SIZE * 3;
         }
-        Time = new Timer(80,this);
+        Time = new Timer(80, this);
         Time.start();
         createApple();
+
+
+    }
+
+    public void ImageChange()//qwertytredfghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    {
+
     }
 
     public void createApple()
     {
-        appleX = new Random().nextInt(20)*DOT_SIZE;
-        appleY = new Random().nextInt(20)*DOT_SIZE;
+        appleX = new Random().nextInt(20) * DOT_SIZE;
+        appleY = new Random().nextInt(20) * DOT_SIZE;
+
+        for (int i = 0; i < snake_segment; i++)
+        {
+            if((x[i] == appleX) && (y[i] == appleY))
+            {
+                createApple();
+
+
+            }
+        }
     }
 
     public void loadImages()
     {
-        ImageIcon iia = new ImageIcon("src/res/apple.png" );
+        ImageIcon iia = new ImageIcon("apple.png");
         apple = iia.getImage();
-        ImageIcon iid = new ImageIcon("src/res/dot.png");
-        dot = iid.getImage();
+
+        ImageIcon iid_chrome = new ImageIcon("dot_chrome.png");
+        dot_chrome = iid_chrome.getImage();
+
+        ImageIcon iid_ya = new ImageIcon("yabrowser.png");
+        dot_ya = iid_ya.getImage();
+
+        ImageIcon iid_fire = new ImageIcon("firefox.png");
+        dot_fire = iid_chrome.getImage();
+
     }
 
     @Override
     protected void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        if(inGame)
+        if (inGame)
         {
-            g.drawImage(apple,appleX,appleY,this);
+            g.drawImage(apple, appleX, appleY, this);
             for (int i = 0; i < snake_segment; i++)
             {
-                g.drawImage(dot,x[i],y[i],this);
+                g.drawImage(dot_chrome, x[i], y[i], this);
             }
         } else
             {
             String str = "Game Over";
-            g.setColor(Color.WHITE);
-            g.drawString(str,320/2-15,SIZE/2);
+            g.setColor(Color.white);
+            g.drawString(str, 320 / 2 - 15, SIZE / 2);
+            score = snake_segment -3;
+            String Score = "Score : " + score;
+            g.setColor(Color.white);
+            g.drawString(Score, 320 / 2 - 8, SIZE / 2 + 11);
         }
+
     }
 
     public void move()
     {
         for (int i = snake_segment; i > 0; i--)
         {
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
-        if(left)
+        if (left)
         {
             x[0] -= DOT_SIZE;
         }
-        if(right)
+        if (right)
         {
             x[0] += DOT_SIZE;
-        } if(up)
+        }
+        if (up)
         {
             y[0] -= DOT_SIZE;
-        } if(down)
+        }
+        if (down)
         {
             y[0] += DOT_SIZE;
         }
@@ -108,7 +142,7 @@ public class GameField extends JPanel implements ActionListener
 
     public void checkApple()
     {
-        if(x[0] == appleX && y[0] == appleY)
+        if (x[0] == appleX && y[0] == appleY)
         {
             snake_segment++;
             createApple();
@@ -117,27 +151,27 @@ public class GameField extends JPanel implements ActionListener
 
     public void checkCollisions()
     {
-        for (int i = snake_segment; i >0 ; i--)
+        for (int i = snake_segment; i > 0; i--)
         {
-            if(i>4 && x[0] == x[i] && y[0] == y[i])
+            if (i > 4 && x[0] == x[i] && y[0] == y[i])
             {
                 inGame = false;
             }
         }
 
-        if(x[0]>SIZE)
+        if (x[0] > SIZE)
         {
             inGame = false;
         }
-        if(x[0]<0)
+        if (x[0] < 0)
         {
             inGame = false;
         }
-        if(y[0]>SIZE)
+        if (y[0] > SIZE)
         {
             inGame = false;
         }
-        if(y[0]<0)
+        if (y[0] < 0)
         {
             inGame = false;
         }
@@ -146,7 +180,7 @@ public class GameField extends JPanel implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        if(inGame)
+        if (inGame)
         {
             checkApple();
             checkCollisions();
@@ -163,26 +197,26 @@ public class GameField extends JPanel implements ActionListener
         {
             super.keyPressed(e);
             int key = e.getKeyCode();
-            if(key == KeyEvent.VK_LEFT && !right)
+            if (key == KeyEvent.VK_LEFT && !right)
             {
                 left = true;
                 up = false;
                 down = false;
             }
-            if(key == KeyEvent.VK_RIGHT && !left)
+            if (key == KeyEvent.VK_RIGHT && !left)
             {
                 right = true;
                 up = false;
                 down = false;
             }
 
-            if(key == KeyEvent.VK_UP && !down)
+            if (key == KeyEvent.VK_UP && !down)
             {
                 right = false;
                 up = true;
                 left = false;
             }
-            if(key == KeyEvent.VK_DOWN && !up)
+            if (key == KeyEvent.VK_DOWN && !up)
             {
                 right = false;
                 down = true;
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class MainWindow extends JFrame
@@ -8,14 +10,53 @@ public class MainWindow extends JFrame
     {
         setTitle("Snake");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(370,390);
+        setSize(370,400);
         setLocation(1920/2-370/2,1080/2-390/2);
         add(new GameField());
-        setVisible(true);
+        setVisible(false);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         MainWindow mw = new MainWindow();
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        jMenuBar.add(file);
+
+        file.add(new JMenuItem("Start", 'S')).addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+            }
+        });
+        file.addSeparator();
+        file.add(new JMenuItem("Exit", 'E')).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        jFrame.setJMenuBar(jMenuBar);
+        jFrame.revalidate();
+
     }
-}
+
+    static JFrame jFrame = getFrame();
+    static JFrame getFrame()
+    {
+        JFrame jFrame = new JFrame() {};
+        jFrame.setTitle("Snake");
+        jFrame.setVisible(true);
+        jFrame.setBounds(1920/2-370/2, 1080/2-390/2, 370, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return jFrame;
+
+    }
+
+
+
+
+    }
